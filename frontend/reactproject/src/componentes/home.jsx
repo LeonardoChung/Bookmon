c985725f0dde41ea7d1d4768d3d4c74268ad380a
Isabella <berkembrockisabella@gmail.com>
2025-05-19 11:55:46 -0300

    Frontend da Página Home finalizado

--- frontend/reactproject/src/componentes/home.jsx
+++ frontend/reactproject/src/componentes/home.jsx
@@ -1,7 +1,12 @@
 import Header from "./header.jsx";
+import book from "../images/book.webp";
+import quackitoWave from "../images/quackito_wave.gif";
+import exampleUser from "../images/example-user.png";
+import "../css/home.css";
 import { Link } from "react-router-dom";
 
 function Home(){
+
 
     return(
         <div className="geral">
@@ -12,9 +17,25 @@ function Home(){
 
             <div className="book">
 
-                <div className="feed"><Link className="link" to="/feed">Feed</Link></div>
-                <div className="user"><Link className="link" to="/user">User</Link></div>
-                <div className="bichinho"><Link className="link" to="/bichinho">Bichinho</Link></div>
+                <img src={book} alt="Book" className="book-img" />
+
+                <div className="feed">
+                    <Link className="link" to="/feed">
+                        <div className="baloon">O que será que os outros estão lendo?</div>
+                    </Link>
+                </div>
+                <div className="user">
+                    <Link className="link" to="/user">
+                        <img src={exampleUser} alt="User" className="exampleUser-img" />
+                    </Link>
+                </div>
+                <div className="bichinho">
+                    <p>Bem-vindo, leitor!</p>
+                    <p>Vamos ler?</p>
+                    <Link className="link" to="/bichinho">
+                        <img src={quackitoWave} alt="Quackito" className="quackito-img" />
+                    </Link>
+                </div>
 
             </div>
    
