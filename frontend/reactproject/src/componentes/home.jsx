83c5c555a7242d2c1a30762dc997a446d10e265d
Isabella <berkembrockisabella@gmail.com>
2025-05-22 21:57:31 -0300

    Melhorei a intuição da página Home

--- frontend/reactproject/src/componentes/home.jsx
+++ frontend/reactproject/src/componentes/home.jsx
@@ -2,6 +2,9 @@ import Header from "./header.jsx";
 import book from "../images/book.webp";
 import quackitoWave from "../images/quackito_wave.gif";
 import exampleUser from "../images/example-user.png";
+import flecha1 from "../images/seta-curva.png";
+import flecha2 from "../images/seta-curva 2.png";
+import flecha3 from "../images/seta-curva 3.png";
 import "../css/home.css";
 import { Link } from "react-router-dom";
 
@@ -9,11 +12,23 @@ function Home(){
 
 
     return(
-        <div className="geral">
+    <div className="geral">
         
         <Header />
 
-        <div className="body">
+        <div className="home-body">
+
+            <div className="home-left">
+                <div className="item-left">
+                    <div className="item-text">Acesse seu feed</div>
+                    <img src={flecha2} alt="flecha2" className="flecha2" />
+                </div>
+                <div className="item-left">
+                    <div className="item-text">Acesse seu perfil</div>
+                    <img src={flecha3} alt="flecha3" className="flecha3" />
+                </div>
+
+            </div>
 
             <div className="book">
 
@@ -37,6 +52,14 @@ function Home(){
                     </Link>
                 </div>
 
+            </div>
+
+            <div className="home-right">
+                <div className="item-right">
+                    <img src={flecha1} alt="flecha1" className="flecha1" />
+                    <div className="item-text">Acesse seu Quackito virtual</div>
+
+                </div>
             </div>
    
         </div>
