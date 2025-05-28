9a51106da58fbc993729f0919ae6524ae7d4861a
Ana Flávia <aflavinhams@gmail.com>
2025-05-28 19:08:53 -0300

    leitura_back

--- backend/controllers/leituras.js
+++ backend/controllers/leituras.js
@@ -1,10 +1,10 @@
 import db from '../db.js';
 
 export const getLeituras = (req, res) => {
-  const q = "SELECT book, pages FROM leituras WHERE iduser = ?";
+  const q = "SELECT idleituras, book, pages FROM leituras WHERE iduser = ?";
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
-    return res.status(200).json(data);  
+    return res.status(200).json(data); 
   });
 };
 
@@ -23,7 +23,7 @@ export const updateLeitura = (req, res) => {
     const { book, pages } = req.body;
     const id = req.params.id;
 
-    const q = "UPDATE leituras SET book = ?, pages = ? WHERE id = ?";
+    const q = "UPDATE leituras SET book = ?, pages = ? WHERE idleituras = ?";
     db.query(q, [book, pages, id], (err, result) => {
         if (err) return res.status(500).json(err);
         return res.status(200).json({ message: "Leitura atualizada com sucesso!" });
--- frontend/reactproject/src/componentes/leituras.jsx
+++ frontend/reactproject/src/componentes/leituras.jsx
@@ -5,6 +5,8 @@ function Leituras() {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
   const { id } = useParams();
+  const [isEditModal, setIsEditModal] = useState(false);
+  const [editedLivro, setEditedLivro] = useState({});
 
   useEffect(() => {
     fetch(`http://localhost:3001/leituras/getLeituras/${id}`)
@@ -12,6 +14,45 @@ function Leituras() {
       .then((dados) => {setData(dados);})
       .catch((erro) => console.error("Erro ao buscar leituras:", erro));
   }, [id]);
+
+  function handleEdit(item) {
+    setEditedLivro({...item});
+    setIsEditModal(true);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setEditedLivro((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleSaveEdit() {
+    console.log(editedLivro);
+    console.log("EDITANDO:", {
+  id: editedLivro.idleituras,
+  book: editedLivro.book,
+  pages: editedLivro.pages
+});
+    fetch(`http://localhost:3001/leituras/${editedLivro.idleituras}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+      book: editedLivro.book,
+      pages: editedLivro.pages
+    }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        console.log(`Leitura ${editedLivro.book} atualizada.`);
+        alert(`Leitura ${editedLivro.idleituras} atualizada.`);
+        setIsEditModal(false);
+        setData((prevData) =>
+          prevData.map((livro) =>
+            livro.idleituras === editedLivro.idleituras ? editedLivro : livro
+          )
+        );
+      })
+      .catch((error) => console.error("Erro ao atualizar:", error));
+  }
 
   return (
     <div>
@@ -25,24 +66,46 @@ function Leituras() {
 
       <ul className="list">
         {data.map((item) => (
-          <li key={item.id} className="li-list">
+          <li key={item.idleituras} className="li-list">
             Nome: {item.book}
             <br />
             Páginas: {item.pages}
             <br />
-            <button
-              onClick={() => navigate(`/editar/${item.id}`)}
-              className="btn-list"
-            >
+            <button onClick={() => handleEdit(item)} className="list-btn">
               Editar
             </button>
           </li>
         ))}
       </ul>
 
-      {/* 
-        update -> `http://localhost:3001/leituras/${id}`
-      */}
+      {isEditModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Editar Leitura</h2>
+            <label>Nome:</label>
+            <input
+              type="text"
+              name="book"
+              value={editedLivro.book}
+              onChange={handleInputChange}
+            />
+            <label>Páginas:</label>
+            <input
+              type="text"
+              name="pages"
+              value={editedLivro.pages}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleSaveEdit} className="list-btn">
+              Salvar
+            </button>
+            <button onClick={() => setIsEditModal(false)} className="list-btn">
+              Cancelar
+            </button>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
