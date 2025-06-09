2e529121da7c4526918d0c844d75b0718414f673
Michele Otta <micheleotta@gmail.com>
2025-06-08 23:19:55 -0300

    conquistas

--- backend/controllers/conquistas.js
+++ backend/controllers/conquistas.js
@@ -1,6 +1,6 @@
 import db from '../db.js';
 
-// Conquista 1 50 paginas
+// Conquista 50 paginas
 export const getConquistaPaginas = (req, res) => {
     const q ='SELECT idconquistas, name, description FROM conquistas;';
     db.query(q, [req.params.id], (err, data) => {
@@ -27,3 +27,35 @@ export const completeConquistaPaginas = (req, res) => {
     }
   });
 };
+
+// Nível 3 do Quackito
+export const getConquistaQuackito = (req, res) => {
+    const q = `SELECT \`status\` FROM user_conq WHERE iduser = ? AND idconquista = 2;`;
+    db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data); 
+});
+};
+
+export const completeConquistaQuackito = (req, res) => {
+    const iduser = req.params.id;
+    const q = `SELECT IFNULL(SUM(pages), 0) AS total FROM leituras WHERE iduser = ?`;
+    db.query(q, [iduser], (err, result) => {
+    if (err) return res.status(500).json(err);
+    const total = result[0].total;
+  
+
+    if (total >= 50) {
+    const update = `UPDATE user_conq SET status = 1 WHERE iduser = ? AND idconquista = 1 AND status = 0`;
+    
+      db.query(update, [iduser], (err) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json({ message: " Conquista alcançada!" });
+      });
+    }
+  });
+};
+
+// 10 livros
+
+// posts
